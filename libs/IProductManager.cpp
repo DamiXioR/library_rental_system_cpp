2c172475da7d5aa91b4ad6605dbfda8c25cb90ea
damixiorek <damixiorek@gmail.com>
2020-12-04 18:32:07 +0100

    init basics add and removeProduct() methods in IProductManager

--- libs/IProductManager.cpp
+++ libs/IProductManager.cpp
@@ -14,7 +14,17 @@ IProductManager::~IProductManager()
 
 void IProductManager::addProduct(IProduct* libProduct)
 {
+    productList_->emplace_back(std::move(*libProduct));
 }
 void IProductManager::removeProduct(IProduct* libProduct)
 {
+    //TO DO COPY CTOR FOR IPRODUCT
+    /*
+    if (!productList_->empty()) {
+        auto foundedAtPosition = std::find(productList_->begin(), productList_->end(), libProduct);
+        if (foundedAtPosition != productList_->end()) {
+            productList_->erase(foundedAtPosition);
+        }
+    }
+    */
 }
