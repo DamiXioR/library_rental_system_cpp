e0990952f379336b0609439f8a13c26e0e1b1fbb
damixiorek <damixiorek@gmail.com>
2020-12-05 18:00:48 +0100

    add more tests to LibProductManager for add and remove IProduct function

--- tests/LibProductManager-ut.cpp
+++ tests/LibProductManager-ut.cpp
@@ -31,38 +31,63 @@ public:
     std::string authorTerryPratchett = "Terry Pratchet";
     std::string publisherDoubleday = "Doubleday";
     std::string year2008 = "2008";
-};
 
-TEST_F(LibProductManagerTest, ShouldLoadVectorOfIProducts)
-{
+    //Books
+
     Book* diunaBook = new Book(titleDiuna, authorFrankHerbert, typeSciFi, publisherChiltonBooks, year1965);
     Book* hyperionBook = new Book(titleHyperion, authorDanSimmons, typeSciFi, publisherAmber, year1989);
+    Book* panLodOgrBook = new Book(titlePanLodOgr, authorJaroslawGrzedowicz, typeSciFi, publisherFabrykaSlow, year2005);
+    Book* nationBook = new Book(titleNation, authorTerryPratchett, typeSciFi, publisherDoubleday, year2008);
+
+    //Products containers
 
     std::vector<IProduct*>* productListInit = new std::vector<IProduct*>{
         diunaBook,
         hyperionBook};
 
+    ~LibProductManagerTest()
+    {
+        delete diunaBook;
+        delete hyperionBook;
+        delete panLodOgrBook;
+        delete nationBook;
+    }
+};
+
+TEST_F(LibProductManagerTest, ShouldLoadVectorOfIProducts)
+{
+    LibProductManager* libProdMan = new LibProductManager(productListInit);
+    const unsigned productListSize2 = 2;
+
+    delete libProdMan;
+}
+
+TEST_F(LibProductManagerTest, ShouldAddProductsToProductVector)
+{
     LibProductManager* libProdMan = new LibProductManager(productListInit);
 
-    Book* panLodOgrBook = new Book(titlePanLodOgr, authorJaroslawGrzedowicz, typeSciFi, publisherFabrykaSlow, year2005);
-
     libProdMan->addProduct(panLodOgrBook);
-
     const unsigned productListSize3 = 3;
-
     EXPECT_EQ(libProdMan->getProductList()->size(), productListSize3);
 
-    Book* nationBook = new Book(titleNation, authorTerryPratchett, typeSciFi, publisherDoubleday, year2008);
-
     libProdMan->addProduct(nationBook);
-
     const unsigned productListSize4 = 4;
-
     EXPECT_EQ(libProdMan->getProductList()->size(), productListSize4);
 
-    delete diunaBook;
-    delete hyperionBook;
-    delete panLodOgrBook;
-    delete nationBook;
+    delete libProdMan;
+}
+
+TEST_F(LibProductManagerTest, ShouldRemoveProductsFromProductVector)
+{
+    LibProductManager* libProdMan = new LibProductManager(productListInit);
+
+    libProdMan->removeProduct(hyperionBook);
+    const unsigned productListSize1 = 1;
+    EXPECT_EQ(libProdMan->getProductList()->size(), productListSize1);
+
+    libProdMan->removeProduct(diunaBook);
+    const unsigned productListSize0 = 0;
+    EXPECT_EQ(libProdMan->getProductList()->size(), productListSize0);
+
     delete libProdMan;
 }
