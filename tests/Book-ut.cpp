2c0e87813c82d35ca5a6747c64f9d77d2de68d2e
damixiorek <damixiorek@gmail.com>
2020-12-05 16:10:00 +0100

    remove id tests in Book tests - productid should be modified in future

--- tests/Book-ut.cpp
+++ tests/Book-ut.cpp
@@ -18,19 +18,6 @@ public:
     std::string authorDanSimmons = "Dan Simmons";
     std::string publisherAmber = "Amber";
     std::string year1989 = "1989";
-
-    //Product ID
-    unsigned idFirst = 1;
-    unsigned idSecond = 2;
-    unsigned idThird = 3;
-    unsigned idFourth = 4;
-    unsigned idFifth = 5;
-    unsigned idSixth = 6;
-    unsigned idSeventh = 7;
-    unsigned idEight = 8;
-    unsigned idNineth = 9;
-    unsigned idTenth = 10;
-    unsigned idEleventh = 11;
 };
 
 TEST_F(BookTest, ShouldReturnBookFeatures)
@@ -42,7 +29,6 @@ TEST_F(BookTest, ShouldReturnBookFeatures)
     EXPECT_EQ(diunaBook->getLiteratureType(), typeSciFi);
     EXPECT_EQ(diunaBook->getPublisher(), publisherChiltonBooks);
     EXPECT_EQ(diunaBook->getYearOfProduction(), year1965);
-    EXPECT_EQ(IProduct::productId_, idFirst);
 
     delete diunaBook;
 
@@ -53,13 +39,11 @@ TEST_F(BookTest, ShouldReturnBookFeatures)
     EXPECT_EQ(hyperionBook->getLiteratureType(), typeSciFi);
     EXPECT_EQ(hyperionBook->getPublisher(), publisherAmber);
     EXPECT_EQ(hyperionBook->getYearOfProduction(), year1989);
-    EXPECT_EQ(IProduct::productId_, idSecond);
 
     delete hyperionBook;
 
     diunaBook = new Book(titleDiuna, authorFrankHerbert, typeSciFi, publisherChiltonBooks, year1965);
     everyBook = diunaBook;
-    EXPECT_EQ(IProduct::productId_, idThird);
 
     delete diunaBook;
 }
@@ -74,16 +58,12 @@ TEST_F(BookTest, ShouldInitObjectWhenUsingMoveCtor)
     EXPECT_EQ(diunaBook.getLiteratureType(), typeSciFi);
     EXPECT_EQ(diunaBook.getPublisher(), publisherChiltonBooks);
     EXPECT_EQ(diunaBook.getYearOfProduction(), year1965);
-    EXPECT_EQ(diunaBook.getDocumentId(), idFourth);
-    EXPECT_EQ(IProduct::productId_, idFifth);
 
     EXPECT_EQ(secondDiunaBook.getTitle(), titleDiuna);
     EXPECT_EQ(secondDiunaBook.getAuthor(), authorFrankHerbert);
     EXPECT_EQ(secondDiunaBook.getLiteratureType(), typeSciFi);
     EXPECT_EQ(secondDiunaBook.getPublisher(), publisherChiltonBooks);
     EXPECT_EQ(secondDiunaBook.getYearOfProduction(), year1965);
-    EXPECT_EQ(secondDiunaBook.getDocumentId(), idFourth);
-    EXPECT_EQ(IProduct::productId_, idFifth);
 }
 
 TEST_F(BookTest, ShouldInitObjectWhenUsingMoveAsignmentOperator)
@@ -97,16 +77,12 @@ TEST_F(BookTest, ShouldInitObjectWhenUsingMoveAsignmentOperator)
     EXPECT_EQ(diunaBook.getLiteratureType(), typeSciFi);
     EXPECT_EQ(diunaBook.getPublisher(), publisherChiltonBooks);
     EXPECT_EQ(diunaBook.getYearOfProduction(), year1965);
-    EXPECT_EQ(diunaBook.getDocumentId(), idSixth);
-    EXPECT_EQ(IProduct::productId_, idSeventh);
 
     EXPECT_EQ(oldDestroyedBook.getTitle(), titleDiuna);
     EXPECT_EQ(oldDestroyedBook.getAuthor(), authorFrankHerbert);
     EXPECT_EQ(oldDestroyedBook.getLiteratureType(), typeSciFi);
     EXPECT_EQ(oldDestroyedBook.getPublisher(), publisherChiltonBooks);
     EXPECT_EQ(oldDestroyedBook.getYearOfProduction(), year1965);
-    EXPECT_EQ(oldDestroyedBook.getDocumentId(), idSixth);
-    EXPECT_EQ(IProduct::productId_, idSeventh);
 }
 
 TEST_F(BookTest, ShouldInitObjectWhenUsingMoveCtorWhenInitWithPointer)
@@ -119,16 +95,12 @@ TEST_F(BookTest, ShouldInitObjectWhenUsingMoveCtorWhenInitWithPointer)
     EXPECT_EQ(diunaBook->getLiteratureType(), typeSciFi);
     EXPECT_EQ(diunaBook->getPublisher(), publisherChiltonBooks);
     EXPECT_EQ(diunaBook->getYearOfProduction(), year1965);
-    EXPECT_EQ(diunaBook->getDocumentId(), idEight);
-    EXPECT_EQ(IProduct::productId_, idNineth);
 
     EXPECT_EQ(oldDestroyedBook->getTitle(), titleDiuna);
     EXPECT_EQ(oldDestroyedBook->getAuthor(), authorFrankHerbert);
     EXPECT_EQ(oldDestroyedBook->getLiteratureType(), typeSciFi);
     EXPECT_EQ(oldDestroyedBook->getPublisher(), publisherChiltonBooks);
     EXPECT_EQ(oldDestroyedBook->getYearOfProduction(), year1965);
-    EXPECT_EQ(oldDestroyedBook->getDocumentId(), idEight);
-    EXPECT_EQ(IProduct::productId_, idNineth);
 
     delete diunaBook;
     delete oldDestroyedBook;
@@ -145,16 +117,12 @@ TEST_F(BookTest, ShouldInitObjectWhenUsingMoveAsignmentOperatorWhenInitWithPoint
     EXPECT_EQ(diunaBook->getLiteratureType(), typeSciFi);
     EXPECT_EQ(diunaBook->getPublisher(), publisherChiltonBooks);
     EXPECT_EQ(diunaBook->getYearOfProduction(), year1965);
-    EXPECT_EQ(diunaBook->getDocumentId(), idTenth);
-    EXPECT_EQ(IProduct::productId_, idEleventh);
 
     EXPECT_EQ(oldDestroyedBook->getTitle(), titleDiuna);
     EXPECT_EQ(oldDestroyedBook->getAuthor(), authorFrankHerbert);
     EXPECT_EQ(oldDestroyedBook->getLiteratureType(), typeSciFi);
     EXPECT_EQ(oldDestroyedBook->getPublisher(), publisherChiltonBooks);
     EXPECT_EQ(oldDestroyedBook->getYearOfProduction(), year1965);
-    EXPECT_EQ(oldDestroyedBook->getDocumentId(), idTenth);
-    EXPECT_EQ(IProduct::productId_, idEleventh);
 
     delete diunaBook;
     delete oldDestroyedBook;
